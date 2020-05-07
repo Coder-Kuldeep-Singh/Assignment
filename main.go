38ffc635c952eab59be93a373358c599316078b3
mavensingh <kuldeep.avsar@gmail.com>
2020-05-07 13:54:06 +0530

    modified the sql

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"emailsender/handler"
+	"emailsender/models"
 	"fmt"
 	"sync"
 	"time"
@@ -9,6 +10,10 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
+	usertimezone := models.GetLocation()
+	if usertimezone == nil {
+		return
+	}
 	ticker := time.NewTicker(1 * time.Second)
 	quit := make(chan struct{})
 	wg.Add(1)
@@ -16,7 +21,7 @@ func main() {
 		for {
 			select {
 			case <-ticker.C:
-				go handler.Execute("Good Morning", &wg)
+				go handler.Execute("Good Morning", usertimezone, &wg)
 			case <-quit:
 				ticker.Stop()
 				return
